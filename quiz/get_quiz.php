4797514318d44727a2db5e041a444ed6f98d910a
Ben_Lin <benlin96186@gmail.com>
2025-08-13 15:51:49 +0800

    feat- quiz get, patch api

---
+++ quiz/get_quiz.php
@@ -0,0 +1,24 @@
+<?php
+  require_once("../common/cors.php");
+  require_once("../common/conn.php");
+
+  if($_SERVER['REQUEST_METHOD']=="GET"){
+
+    $sql = "SELECT * FROM quizzes;";
+
+    $stmt = $mysqli->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $response_data = $result->fetch_all(MYSQLI_ASSOC);
+    
+
+    echo json_encode($response_data);
+    $mysqli->close();
+    exit();
+
+  }
+  http_response_code(403);
+  $reply_data = new stdClass();
+  $reply_data->error = "拒絕存取";
+  echo json_encode($reply_data);
+?>
