7f3c739b20b49ace3d39fd925b923d4bc31c35bf
Ben_Lin <benlin96186@gmail.com>
2025-08-14 19:15:49 +0800

    fix- update get_questions

--- questions/get_questions.php
+++ questions/get_questions.php
@@ -3,23 +3,29 @@
   require_once("../common/conn.php");
 
   if($_SERVER['REQUEST_METHOD']=="GET"){
-    $response_data = new stdClass();
 
     $sql = "SELECT * FROM questions;";
-
     $stmt = $mysqli->prepare($sql);
     $stmt->execute();
-    $result = $stmt->get_result();
-    $questions = $result->fetch_all(MYSQLI_ASSOC);
-    $response_data->questions = $questions;
 
-    $sql = "SELECT quiz_id, title FROM quizzes;";
+    $stmt->bind_result($question_id, $quiz_id, $question_description,$option_1, $option_2, $option_3, $answer, $explanation);
 
-    $stmt = $mysqli->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $quiz_title = $result->fetch_all(MYSQLI_ASSOC);
-    $response_data->quiz_title = $quiz_title;
+    $response_data = []; 
+
+    while ($stmt->fetch()) {
+        $row = [
+            'question_id' => $question_id,
+            'quiz_id' => $quiz_id,
+            'question_description' => $question_description,
+            'option_1' => $option_1,
+            'option_2' => $option_2,
+            'option_3' => $option_3,
+            'answer' => $answer,
+            'explanation' => $explanation
+        ];
+        $response_data[] = $row; 
+    }
+    $stmt->close();
 
     echo json_encode($response_data);
     $mysqli->close();
@@ -30,4 +36,7 @@
   $reply_data = new stdClass();
   $reply_data->error = "拒絕存取";
   echo json_encode($reply_data);
+
+
 ?>
+
